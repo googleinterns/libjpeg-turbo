fe4ae3feedee0450e2799b2f44d212f78c029cec
Adan Lopez <adanlopez@google.com>
2020-06-24 20:33:00 -0500

    Added matrix-multiply dct along with unit test

--- xla/dctunittest.c
+++ xla/dctunittest.c
@@ -1,44 +1,28 @@
 /*
- * jfdctxla.c
+ * dctunittest.c
  *
- * Copyright (C) 1994-1996, Thomas G. Lane.
- * This file is part of the Independent JPEG Group's software.
- * For conditions of distribution and use, see the accompanying README.ijg
- * file.
- *
- * Modification by Adan Lopez under Google 2020.
- *
- * This file contains a floating-point implementation of the
- * forward DCT (Discrete Cosine Transform).
- *
- * It uses classic matrix multiplication, which does more overall arithmetic
- * operations than the optimized AA&M algorithm (in jfdctflt.c). However,
- * Google's XLA, Tensorflow, and TPU provide a processor infrastructure specialized
- * for linear algebra operations. Therefore, the implementation of DCT is highly optimized.
- *
+ * Since xla only runs dct, unit testing is done by generating random 8x8 blocks,
+ * running them on both xla and the original dct, and comparing the outputs.
  */
 
 #define JPEG_INTERNALS
 #include "../jinclude.h"
 #include "../jpeglib.h"
-#include "../jdct.h"               /* Private declarations for DCT subsystem */
-#include <string.h>
+#include "../jdct.h"
+#include <time.h>
 
-#define abs(x) (x > 0? x: -x)
-
-/*
- * This module is specialized to the case DCTSIZE = 8.
- */
+#define abs(x) ((x) > 0? (x): -(x))
+#define max(x, y) (x > y? x: y)
 
 #if DCTSIZE != 8
-  Sorry, this code only copes with 8x8 DCTs. /* deliberate syntax err */
+  Sorry, this code only copes with 8x8 DCTs.
 #endif
 
 static boolean equivalent(FAST_FLOAT *data1, FAST_FLOAT *data2) {
 	int i, j;
 	for(i = 0; i < DCTSIZE; i++, data1 += DCTSIZE, data2 += DCTSIZE)
 		for(j = 0; j < DCTSIZE; j++)
-			if(abs(data1[j] - data2[j]) > (FAST_FLOAT) ERROR_MARGIN)
+			if(abs(data1[j] - data2[j]) > abs(max(data1[j], data2[j])) * (FAST_FLOAT) REL_ERROR_MARGIN)
 				return 0;
 	return 1;
 }
@@ -46,7 +30,7 @@ static boolean equivalent(FAST_FLOAT *data1, FAST_FLOAT *data2) {
 static boolean test_block(FAST_FLOAT *data) {
 	FAST_FLOAT normal_dct[DCTSIZE * DCTSIZE];
 	FAST_FLOAT xla_dct[DCTSIZE * DCTSIZE];
-
+random 
 	memcpy(normal_dct, data, sizeof normal_dct);
 	memcpy(xla_dct, data, sizeof xla_dct);
 
@@ -56,27 +40,33 @@ static boolean test_block(FAST_FLOAT *data) {
 	return equivalent(normal_dct, xla_dct);
 }
 
+/*
+ * Create an 8x8 matrix with random values.
+ */
 static void fill_block(FAST_FLOAT *data) {
 	int i, j;
 	for(i = 0; i < DCTSIZE; i++, data += DCTSIZE)
-		for(j = 0; j < DCTSIZE; j++)
-			data[j] = (FAST_FLOAT) 1.33345;
+		for(j = 0; j < DCTSIZE; j++) {
+			data[j] = (FAST_FLOAT) rand() / RAND_MAX * 200;
+		}
 }
-
-/*
- * Perform the forward DCT on one block of samples.
- */
 
 int main(int argc, char *argv[])
 {
 	FAST_FLOAT block[DCTSIZE * DCTSIZE];
-	fill_block(block);
-	if(test_block(block)) {
-		fprintf(stderr, "Test succeeded.\n");
-		exit(EXIT_SUCCESS);
-	} else {
-		fprintf(stderr, "Test failed: xla's dct produced a different result than original dct\n");
-		exit(EXIT_FAILURE);
+	int i;
+	srand((unsigned)time(NULL));
+
+	for(int i = 0; i < TEST_COUNT; i++) {
+		fill_block(block);
+		if(test_block(block)) {
+			fprintf(stderr, "Test succeeded.\n");
+		} else {
+			fprintf(stderr, "Test failed: xla's dct produced a different result than original dct\n");
+			exit(EXIT_FAILURE);
+		}
 	}
+	fprintf(stderr, "All tests succeeded.\n");
+	exit(EXIT_SUCCESS);
 }
 
--- xla/jfdctxla.c
+++ xla/jfdctxla.c
@@ -1,32 +1,92 @@
 /*
  * jfdctxla.c
  *
- * Copyright (C) 1994-1996, Thomas G. Lane.
- * This file is part of the Independent JPEG Group's software.
- * For conditions of distribution and use, see the accompanying README.ijg
- * file.
- *
  * Modification by Adan Lopez under Google 2020.
  *
  * Stub forward DCT (Discrete Cosine Transform) for XLA.
  *
  */
 
+#define JPEG_INTERNALS
 #include "jinclude.h"
 #include "jpeglib.h"
-#include "jdct.h"               /* Private declarations for DCT subsystem */
-
-/*
- * This module is specialized to the case DCTSIZE = 8.
- */
+#include "jdct.h"
+#include <math.h>
 
 #if DCTSIZE != 8
-  Sorry, this code only copes with 8x8 DCTs. /* deliberate syntax err */
+  Sorry, this code only copes with 8x8 DCTs.
 #endif
+
+FAST_FLOAT dct_matrix[DCTSIZE * DCTSIZE] = {
+	0.353553391, 0.353553391, 0.353553391, 0.353553391, 0.353553391, 0.353553391, 0.353553391, 0.353553391,
+	0.490392640, 0.415734806, 0.277785117, 0.097545161, -0.097545161, -0.277785117, -0.415734806, -0.490392640,
+	0.461939766, 0.191341716, -0.191341716, -0.461939766, -0.461939766, -0.191341716, 0.191341716, 0.461939766,
+	0.415734806, -0.097545161, -0.490392640, -0.277785117, 0.277785117, 0.490392640, 0.097545161, -0.415734806,
+	0.353553391, -0.353553391, -0.353553391, 0.353553391, 0.353553391, -0.353553391, -0.353553391, 0.353553391,
+	0.277785117, -0.490392640, 0.097545161, 0.415734806, -0.415734806, -0.097545161, 0.490392640, -0.277785117,
+	0.191341716, -0.461939766, 0.461939766, -0.191341716, -0.191341716, 0.461939766, -0.461939766, 0.191341716,
+	0.097545161, -0.277785117, 0.415734806, -0.490392640, 0.490392640, -0.415734806, 0.277785117, -0.097545161
+};
+
+FAST_FLOAT transposed_dct_matrix[DCTSIZE * DCTSIZE] = {
+	0.353553391, 0.490392640, 0.461939766, 0.415734806, 0.353553391, 0.277785117, 0.191341716, 0.097545161,
+	0.353553391, 0.415734806, 0.191341716, -0.097545161, -0.353553391, -0.490392640, -0.461939766, -0.277785117,
+	0.353553391, 0.277785117, -0.191341716, -0.490392640, -0.353553391, 0.097545161, 0.461939766, 0.415734806,
+	0.353553391, 0.097545161, -0.461939766, -0.277785117, 0.353553391, 0.415734806, -0.191341716, -0.490392640,
+	0.353553391, -0.097545161, -0.461939766, 0.277785117, 0.353553391, -0.415734806, -0.191341716, 0.490392640,
+	0.353553391, -0.277785117, -0.191341716, 0.490392640, -0.353553391, -0.097545161, 0.461939766, -0.415734806,
+	0.353553391, -0.415734806, 0.191341716, 0.097545161, -0.353553391, 0.490392640, -0.461939766, 0.277785117,
+	0.353553391, -0.490392640, 0.461939766, -0.415734806, 0.353553391, -0.277785117, 0.191341716, -0.097545161
+};
+
+FAST_FLOAT flow_coeff[DCTSIZE * DCTSIZE] = {
+	8.000000,  11.096321, 10.452571, 9.406975,  8.000002,  6.285559,  4.329570,  2.207195,
+	11.096320, 15.391046, 14.498035, 13.047892, 11.096315, 8.718322,  6.005284,  3.061468,
+	10.452501, 14.498041, 13.656854, 12.290866, 10.452503, 8.212481,  5.656853,  2.883838,
+	9.407003,  13.047894, 12.290842, 11.061483, 9.407002,  7.391037,  5.091034,  2.595389,
+	8.000001,  11.096319, 10.452504, 9.407007,  8.000000,  6.285560,  4.329569,  2.207195,
+	6.285562,  8.718323,  8.212480,  7.391035,  6.285561,  4.938532,  3.401721,  1.734182,
+	4.329566,  6.005280,  5.656854,  5.091043,  4.329567,  3.401721,  2.343146,  1.194525,
+	2.207195,  3.061468,  2.883839,  2.595387,  2.207199,  1.734182,  1.194525,  0.608964
+};
+
+FAST_FLOAT tmp[DCTSIZE * DCTSIZE];
+
+/*
+ * Multiplies 8x8 matrices matrix1 and matrix2, and writes the answer in result,
+ */
+void matrix_multiply(FAST_FLOAT *matrix1, FAST_FLOAT *matrix2, FAST_FLOAT *result) {
+	/* It's possible that result pointer is the same as matrix1 or matrix2. Therefore, writing the
+	 * answer directly to result is unsafe. So we store the answer in aux during the multiplication,
+	 * and copy it to result in the end.
+	 */
+	int i, j, k;
+	FAST_FLOAT operand1, operand2, *current_cell;
+
+	for(i = 0; i < DCTSIZE; i++) {
+		for(j = 0; j < DCTSIZE; j++) {
+			current_cell = tmp + i * DCTSIZE + j;
+			*current_cell = 0;
+			for(k = 0; k < DCTSIZE; k++) {
+				operand1 = matrix1[i * DCTSIZE + k];
+				operand2 = matrix2[k * DCTSIZE + j];
+
+				*current_cell += operand1 * operand2;
+			}
+		}
+	}
+	memcpy(result, tmp, sizeof tmp);
+}
 
 GLOBAL(void)
 jpeg_fdct_xla(FAST_FLOAT *data)
 {
-
+	fprintf(stderr, "In xla dct\n");
+	matrix_multiply(dct_matrix, data, data);
+	matrix_multiply(data, transposed_dct_matrix, data);
+	
+	int i;
+	for(int i = 0; i < DCTSIZE * DCTSIZE; i++)
+		data[i] *= flow_coeff[i];
 }
 
